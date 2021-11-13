eb4a8d2bd4391238961bc9be2554e3ac10b99ccd
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-11-13 20:26:18 +0200

    Ex 3.1.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+          {
+            "name": "Arto Hellas",
+            "number": "040-123456",
+            "id": 1
+          },
+          {
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523",
+            "id": 2
+          },
+          {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+            "id": 4
+          }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
