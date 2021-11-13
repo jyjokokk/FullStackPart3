0ec766f3390c6458b407070ca01909ff382e24d9
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-11-13 20:36:23 +0200

    Ex 3.2.

--- index.js
+++ index.js
@@ -4,28 +4,35 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-          {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-          },
-          {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-          },
-          {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122",
-            "id": 4
-          }
+	{
+		name: 'Arto Hellas',
+		number: '040-123456',
+		id: 1
+	},
+	{
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+		id: 2
+	},
+	{
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+		id: 3
+	}
 ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+	res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+	const message = `Phonebook has info of ${persons.length} people.
+                  <br></br>
+                  ${new Date()}`
+	res.send(message)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+	console.log(`Server running on ${PORT}`)
 })
