a5007af041263964125b772b905fc304a26682e9
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-12-19 08:54:59 +0200

    Ex 3.15-16

--- index.js
+++ index.js
@@ -14,79 +14,77 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response, next) => {
-  Person.find({})
-    .then(persons => {
-      const message = `Phonebook has info of ${persons.length} people.
+	Person.find({})
+		.then((persons) => {
+			const message = `Phonebook has info of ${persons.length} people.
                     <br></br>
                     ${new Date()}`
-      response.send(message)
-    })
-    .catch(error => next(error))
+			response.send(message)
+		})
+		.catch((error) => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-  Person.find({})
-    .then((persons) => {
-      response.json(persons)
-    })
-    .catch(error => next(error))
+	Person.find({})
+		.then((persons) => {
+			response.json(persons)
+		})
+		.catch((error) => next(error))
 })
-
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch((error) => next(error))
 })
-
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end()
+		})
+		.catch((error) => next(error))
 })
 
-
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-  console.log(body.name + " " + body.number)
+	const body = request.body
+	console.log(body.name + ' ' + body.number)
 	if (!body.name || !body.number) {
 		return response.status(400).json({
 			error: 'Request must contain name and number.'
 		})
 	}
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-  person.save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
+	person
+		.save()
+		.then((savedPerson) => {
+			response.json(savedPerson)
+		})
+		.catch((error) => next(error))
 })
 
-
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'Unknown endpoint'})
+	response.status(404).send({ error: 'Unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'Malformed id' })
-  }
-  next(error)
+	console.error(error.message)
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'Malformed id' })
+	}
+	next(error)
 }
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
