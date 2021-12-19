36478fe1fa4b5a356baa67729b510bc4a7f46523
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-12-19 04:15:06 +0200

    Ex 3.12.

--- index.js
+++ index.js
@@ -29,12 +29,10 @@ let persons = [
 	}
 ]
 
-
 const generateId = (min, max) => {
-  let id = Math.floor(Math.random() * (max - min) + min)
-  return id
+	let id = Math.floor(Math.random() * (max - min) + min)
+	return id
 }
-
 
 app.get('/info', (req, res) => {
 	const message = `Phonebook has info of ${persons.length} people.
@@ -43,52 +41,47 @@ app.get('/info', (req, res) => {
 	res.send(message)
 })
 
-
 app.get('/api/persons', (req, res) => {
 	res.json(persons)
 })
 
-
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
+	const id = Number(req.params.id)
+	const person = persons.find((person) => person.id === id)
 
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
 })
-
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end();
+	const id = Number(req.params.id)
+	persons = persons.filter((person) => person.id !== id)
+	res.status(204).end()
 })
-
 
 app.post('/api/persons', (req, res) => {
-  const body = req.body
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: 'Request must contain name and number.'
-    })
-  }
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(409).json({
-      error: 'Duplicate data item'
-    })
-  }
-  const person = {
-    name: body.name,
-    number: body.number || "",
-    id: generateId(4, 120)
-  }
-  persons = persons.concat(person)
-  res.json(person)
+	const body = req.body
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: 'Request must contain name and number.'
+		})
+	}
+	if (persons.find((person) => person.name === body.name)) {
+		return res.status(409).json({
+			error: 'Duplicate data item'
+		})
+	}
+	const person = {
+		name: body.name,
+		number: body.number || '',
+		id: generateId(4, 120)
+	}
+	persons = persons.concat(person)
+	res.json(person)
 })
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
